ade8d7bb6a0213a7fc8415be785d73464ffcc90b
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-01-16 18:37:16 +0530

    my wishlist component product grid component

---
+++ src/app/pages/products-grid/products-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-grid',
+  imports: [],
+  templateUrl: './products-grid.html',
+  styleUrl: './products-grid.scss',
+})
+export class ProductsGrid {
+
+}
